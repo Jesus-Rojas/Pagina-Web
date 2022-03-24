cc2dc7d2fd6b77536364fb66719a6a5e40b69924
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-24 15:03:23 -0500

    agregando card en s.o.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'es', includedLanguages: 'ca,eu,gl,en,fr,it,pt,de,es', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true}, 'google_translate_element');
+  new google.translate
+    .TranslateElement(
+      {
+        pageLanguage: 'es', 
+        includedLanguages: 'ca,eu,gl,en,fr,it,pt,de,es', 
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+        gaTrack: true
+      }, 
+      'google_translate_element'
+      );
 }
